926a6651fc6f15a915b9ee878d0789b1d5ab588f
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-10-17 19:05:08 -0500

    Refactor main.dart to use NavbarRoots as the home screen

---
+++ lib/presentation/widgets/navbar_roots.dart
@@ -0,0 +1,60 @@
+import 'package:canchitas/presentation/screens/home_screen.dart';
+import 'package:canchitas/presentation/screens/playgrounds_screen.dart';
+import 'package:canchitas/presentation/screens/profile_screen.dart';
+import 'package:canchitas/presentation/screens/reservations_screen.dart';
+import 'package:flutter/material.dart';
+
+class NavbarRoots extends StatefulWidget {
+  const NavbarRoots({super.key});
+
+  @override
+  State<NavbarRoots> createState() => _NavbarRootsState();
+}
+
+class _NavbarRootsState extends State<NavbarRoots> {
+  int _selectedIndex = 0;
+  final _screens = [
+    const Homescreen(),
+    const PlaygroundsScreen(),
+    const ReservationsScreen(),
+    const ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: SizedBox(
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.pink,
+          currentIndex: _selectedIndex,
+          onTap: (value) {
+            setState(() {
+              _selectedIndex = value;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.sports_soccer),
+              label: 'Playgrounds',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: 'Reservations',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
