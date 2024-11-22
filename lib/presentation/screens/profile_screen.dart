c779b574277234b4f4e4017cf4ff2956b54b4898
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-11-21 19:49:05 -0500

    feat: enhance ProfileScreen with login state management and user details display

--- lib/presentation/screens/profile_screen.dart
+++ lib/presentation/screens/profile_screen.dart
@@ -1,15 +1,18 @@
+import 'package:canchitas/presentation/screens/auth/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
-  _ProfileScreenState createState() => _ProfileScreenState();
+  ProfileScreenState createState() => ProfileScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen> {
-  bool showActiveReservation = true;
+class ProfileScreenState extends State<ProfileScreen> {
+  bool isLoggedIn = false; // Cambia este valor según el estado del usuario
+  String? userName;
+  String? userPhone;
+  String? userAddress;
 
   @override
   Widget build(BuildContext context) {
@@ -17,144 +20,151 @@ class _ProfileScreenState extends State<ProfileScreen> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: IconButton(
-              icon: const Icon(Icons.logout, color: Colors.red),
-              onPressed: () {
-                // Add logout functionality
-              },
-            ),
-          ),
-        ],
+        title: const Text(
+          'Perfil',
+          style: TextStyle(color: Colors.teal),
+        ),
+        centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: isLoggedIn
+            ? _buildProfileContent() // Contenido para usuarios logueados
+            : _buildLoginButton(), // Botón de iniciar sesión
+      ),
+    );
+  }
+
+  Widget _buildProfileContent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Card(
+          elevation: 2,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.grey[300],
+              child: Icon(Icons.person, color: Colors.grey[700]),
+            ),
+            title: Text(
+              userName ?? 'Usuario',
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold, color: Colors.teal),
+            ),
+            subtitle: Text(
+                '${userAddress ?? "Sin dirección"}\n${userPhone ?? "Sin teléfono"}'),
+            trailing: ElevatedButton(
+              onPressed: () {
+                // Agregar funcionalidad para editar
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              child: const Text('Editar'),
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Card(
-              elevation: 2,
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey[300],
-                  child: Icon(Icons.person, color: Colors.grey[700]),
-                ),
-                title: const Text(
-                  'Mario Salvatierra',
-                  style: TextStyle(fontWeight: FontWeight.bold, color: Colors.teal),
-                ),
-                subtitle: const Text('Chorrillos\n920032515'),
-                trailing: ElevatedButton(
-                  onPressed: () {
-                    // Add edit functionality
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                  ),
-                  child: const Text('Editar'),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  // Cambia entre reservas activas y anteriores
+                });
+              },
+              child: const Text(
+                'Reserva activa',
+                style: TextStyle(
+                  color: Colors.teal,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
                 ),
               ),
             ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      showActiveReservation = true;
-                    });
-                  },
-                  child: Text(
-                    'Reserva activa',
-                    style: TextStyle(
-                      color: showActiveReservation ? Colors.teal : Colors.grey,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  // Cambia entre reservas activas y anteriores
+                });
+              },
+              child: const Text(
+                'Reservas anteriores',
+                style: TextStyle(
+                  color: Colors.teal,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
                 ),
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      showActiveReservation = false;
-                    });
-                  },
-                  child: Text(
-                    'Reservas anteriores',
-                    style: TextStyle(
-                      color: showActiveReservation ? Colors.grey : Colors.teal,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ],
+              ),
             ),
-            const Divider(),
-            if (showActiveReservation)
-            // Active Reservation Content
-              ReservationCard(),
-            if (!showActiveReservation)
-            // Previous Reservations Content (showing same info for now)
-              ReservationCard(),
           ],
         ),
+        const Divider(),
+        const ReservationCard(),
+      ],
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'No has iniciado sesión.',
+            style: TextStyle(fontSize: 18, color: Colors.grey),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // Navegar a la pantalla de login
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        const LoginScreen(reservationDetails: {})),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.teal,
+              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+            ),
+            child: const Text(
+              'Iniciar Sesión',
+              style: TextStyle(fontSize: 16, color: Colors.white),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
-// ignore: use_key_in_widget_constructors
 class ReservationCard extends StatelessWidget {
+  const ReservationCard({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return const Card(
       elevation: 2,
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Expanded(
-                  child: Text(
-                    'RESERVA: #3835 - 28 de Julio del 2023\nPRUEBA 512 MIGUEL - Futbol',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                    softWrap: true,
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: const Text('En proceso'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            const Text('Proveedor: PRUEBA 512 MIGUEL'),
-            const Text('Cancha: Futbol'),
-            const Text('Deporte: Fútbol'),
-            const Text('Tamaño de cancha deportiva: 11 vs 11'),
-            const Text('Dirección: AV. calle 23, san borja'),
-            const Text('Fecha: 28 de Julio del 2023'),
-            const Text('Horario de reserva: de 11:00 a 11:59 hrs'),
-            const SizedBox(height: 10),
-            const Text(
-              'Total a pagar: S/ 110.00',
+            Text(
+              'Reserva activa: Cancha de fútbol',
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            const Text('Por pagar: S/ 100.00'),
-            const Text('Pagado: S/ 10.00'),
-            const SizedBox(height: 10),
-            const Text('MÉTODO DE PAGO: App'),
+            SizedBox(height: 10),
+            Text('Fecha: 28 de Julio del 2023'),
+            Text('Horario: 11:00 AM - 12:00 PM'),
+            SizedBox(height: 10),
+            Text(
+              'Total: S/ 110.00',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
           ],
         ),
       ),
